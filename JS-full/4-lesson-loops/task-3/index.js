723f3c0c9019fb61c5a6b0c4e8615c77dc8cfb11
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-01-11 17:29:27 +0200

    .

--- JS-full/4-lesson-loops/task-3/index.js
+++ JS-full/4-lesson-loops/task-3/index.js
@@ -1,6 +1,6 @@
 for (let number = 1; number <= 5; number++) {
   console.log('=> ' + number + ' <=');
   for (let number2 = 0; number2 <= 10; number2++) {
-    console.log(number + ' х ' + number2 + ' = ' + number * number2);
+    console.log(`${number} х ${number2} = ${number * number2}`);
   }
 }
