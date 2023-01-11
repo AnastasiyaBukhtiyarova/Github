f80138a6010706f62994ce1e0114817cf5cc2bae
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-01-11 16:41:45 +0200

    added task

---
+++ JS-full/2-lesson-data-types/task-1-naming/index.js
@@ -0,0 +1,14 @@
+/*
+const User Name = 'John';
+const 1greeting = "I'm John";
+const ho%b@by = 'Walking';
+const FavouriteDESSERT = 'cake';
+const super&Number = 17;
+*/
+
+const userName = 'John';
+const greeting1 = "I'm John";
+const hobby = 'Walking';
+const favourIteDessert = 'cake';
+const superNumber = 17;
+
