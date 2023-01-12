b279331847845bc6cd5626de80e764475c9e5d2c
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-01-12 16:02:23 +0200

    added solution

--- JS-full/4-lesson-eslint/task-2/index.js
+++ JS-full/4-lesson-eslint/task-2/index.js
@@ -1,7 +1,6 @@
-const m = 9
-const n = 12
-let result = 1
-for (let i = m; i <= n; i++) {
-  if (i % 2 === 1) result *= i
+const n = 12;
+let sum = 0;
+for (let counter = 1; counter <= n; counter += 1) {
+  sum += String(counter);
 }
-console.log(result)
+console.log(sum);
