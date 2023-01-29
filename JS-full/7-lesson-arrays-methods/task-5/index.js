a2212ebd5a630abe7fb7d78ac397418dbc31cc10
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-01-29 12:43:30 +0200

    added solution

--- JS-full/7-lesson-arrays-methods/task-5/index.js
+++ JS-full/7-lesson-arrays-methods/task-5/index.js
@@ -1,10 +1,12 @@
-const getMessagesForBestStudents = (allStudentsList, studentsForRetake) => {
-  return allStudentsList
-    .filter((name) => !studentsForRetake.includes(name))
-    .map((student) => 'Good job, ' + student);
+const squareArray = (arr) => {
+  if (!Array.isArray(arr)) return null;
+  return arr.map((num) => num * num);
 };
-// eslint-disable-next-line prettier/prettier
-const allStudents = ['Ann', 'Kate', 'Ed', 'Sheld', 'Tom', 'Li'];
-// eslint-disable-next-line prettier/prettier
-const failedStudents = ['Ann', 'Kate'];
-console.log(getMessagesForBestStudents(allStudents, failedStudents));
+
+const squareArr = [1, 0, 3, 6, 9, 8, 66, 320, 4];
+const squareArr2 = 'Tom is my cat';
+const squareArr3 = [4, 12];
+
+console.log(squareArray(squareArr));
+console.log(squareArray(squareArr2));
+console.log(squareArray(squareArr3));
