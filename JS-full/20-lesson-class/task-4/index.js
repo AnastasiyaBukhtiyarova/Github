fc8ef2bba420b4480a22371ae185e755e5211595
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-04-04 20:38:27 +0300

    solutions added

--- JS-full/20-lesson-class/task-4/index.js
+++ JS-full/20-lesson-class/task-4/index.js
@@ -1,12 +1,47 @@
-export default {
-  firstName: 'Lily',
-  lastName: 'Potter',
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  },
-  set fullName(fullName) {
-    const [firstName, lastName] = fullName.split(' ');
-    this.firstName = firstName;
-    this.lastName = lastName;
-  },
-};
+'use strict';
+
+/**
+ * @field {string} id +
+ * @field {number} price +
+ * @field {date} dateCreated
+ * @field {boolean} isConfirmed +
+ * @field {date} dateConfirmed
+ * @field {string} city +
+ * @field {string} type +
+ */
+class Order {
+  // put your code here
+  constructor(price, city, type, ID) {
+    this.price = price;
+    this.city = city;
+    this.type = type;
+    this.id = Math.random().toString();
+    this.isConfirmed = false;
+    this.dateCreated = new Date();
+    this.dateConfirmed = null;
+  }
+
+  checkPrice() {
+    if (this.price >= 1000) {
+      return true;
+    }
+    return false;
+  }
+
+  confirmOrder() {
+    this.isConfirmed = true;
+    this.dateConfirmed = new Date();
+  }
+
+  isValidType() {
+    if (this.type === 'Buy' || this.type === 'Sell') {
+      return true;
+    }
+    return false;
+  }
+}
+const order = new Order(1500, 'NY', 'Buy', '12');
+console.log(order.checkPrice());
+console.log(order.confirmOrder());
+console.log(order.isValidType());
+export { Order };
