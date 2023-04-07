66c0d90f61b66b6fa1de8200ee5f1010ffa20f6d
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-04-07 22:10:15 +0300

    DOM

--- JS-full/21-lesson-DOM/task-3/index.js
+++ JS-full/21-lesson-DOM/task-3/index.js
@@ -6,8 +6,8 @@ function getItemsList() {
 console.log(getItemsList());
 
 function getItemsArray() {
-  const elementsArray = [...document.querySelectorAll('.tool')];
-  console.log([Array.from(elementsArray)]);
+  const elementsArray = Array.from(document.querySelectorAll('.tool'));
+  console.log(elementsArray);
   return elementsArray;
 }
 console.log(getItemsArray());
