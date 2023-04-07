099065f9b089ec8d7de7150100fd65ac53e0b233
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-04-07 20:42:34 +0300

    DOM find all

--- JS-full/21-lesson-DOM/task-3/index.js
+++ JS-full/21-lesson-DOM/task-3/index.js
@@ -1,1 +1,12 @@
 'use strict';
+const getItemsList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+};
+getItemsList();
+const getItemsArray = () => {
+  const elementsArray = document.querySelectorAll('.tool');
+  Array.from(elementsArray);
+  console.dir(elementsArray);
+};
+getItemsArray();
