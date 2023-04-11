fc2e5718235ca1643bd1a1b4f65721246a1c9410
AB <anabuhtiyarova97@gmail.com>
2023-04-11 15:25:09 +0300

    DOM

--- JS-full/21-lesson-DOM/task-3/index.js
+++ JS-full/21-lesson-DOM/task-3/index.js
@@ -1,26 +1,24 @@
-class Wallet {
-  balance = 0;
+const getTitle = () => {
+  const title = document.querySelector('.title').textContent;
+  return title;
+};
+console.log(getTitle());
 
-  getBalance() {
-    return this.balance;
-  }
+const getDescription = () => {
+  const description = document.querySelector('.about');
+  console.log(description.innerText);
+};
+getDescription();
 
-  deposit(amount) {
-    this.balance += amount;
-  }
+const getPlans = () => {
+  const plans = document.querySelector('.plans');
+  return plans.innerHTML;
+};
+console.log(getPlans());
 
-  withdraw(amount) {
-    if (this.balance < amount) {
-      console.log(`No enough funds`);
-      return;
-    }
-    this.balance -= amount;
-  }
-}
-const wallet = new Wallet();
-console.log(wallet.getBalance(20));
-console.log(wallet.deposit(10));
-console.log(wallet.withdraw(0));
-const wallet1 = new Wallet();
-console.log(wallet1.withdraw(10));
-export { Wallet };
+const getGoal = () => {
+  const goal = document.querySelector('.goal');
+  return goal.outerHTML;
+};
+console.log(getGoal());
+export { getTitle, getDescription, getPlans, getGoal };
--- JS-full/21-lesson-DOM/task-5/index.js
+++ JS-full/21-lesson-DOM/task-5/index.js
@@ -1,51 +1,8 @@
-'use strict';
-class User {
-  constructor(id, name, sessionId) {
-    this._id = id;
-    this._name = name;
-    this._sessionId = sessionId;
-  }
-  get id() {
-    return this._id;
-  }
-  get name() {
-    return this._name;
-  }
-  get sessionId() {
-    return this._sessionId;
-  }
-}
-
-class UserRepository {
-  constructor(users) {
-    this._users = Object.freeze(users);
-  }
-  get users() {
-    return this._users;
-  }
-
-  getUserNames() {
-    return this._users.map((i) => i.name);
-  }
-
-  getUserIds() {
-    return this._users.map((i) => i.id);
-  }
-
-  getUserNameById(id) {
-    const user = this._users.filter((i) => i.id === id).map((i) => i.name)[0];
-    return user;
-  }
-}
-
-const user = new User('1', 'Tom', 'session');
-
-// получить свойства можем
-console.log(user.id); // ===> '1'
-console.log(user.name); // ===> 'Tom'
-console.log(user.sessionId); // ===> 'session-id'
-export { User, UserRepository };
-
-// но изменить эти свойства нельзя
-// user.name = 'Bob'; // пытаемся изменить старое значение
-// console.log(user.name); // ===> 'Tom' - но изменение проигнорировано, так как setter отсутствует
+// put your code here
+const setTitle = (text) => {
+  const findEl = document.querySelector('.title');
+  return (findEl.textContent = text);
+};
+const text = 'text';
+console.log(setTitle(text));
+export { setTitle };
