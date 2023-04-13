c3c6f17828129843e0177936d7c238cb97a88383
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-04-13 21:28:41 +0300

    events

--- JS-full/22-lesson-events/task-1/index.js
+++ JS-full/22-lesson-events/task-1/index.js
@@ -1,11 +1,22 @@
-const el = document.querySelector('.rect-div');
-const div = (text, color) => {
-  const element = document.querySelector('.events-list');
-  element.innerHTML += `<span style= 'color: ${color}; margin-left: 8px;' >'DIV'</span>`;
-  return element;
-};
-const green = div('green');
-const grey = div('grey');
+const divEl = document.querySelector('.rect_div');
+const pEl = document.querySelector('.rect_p');
+const spanEl = document.querySelector('.rect_span');
 
-el.addEventListener('click', green);
-el.addEventListener('click', grey);
+const clicked = (text, color) => {
+  const element = document.querySelector('.events-list');
+  element.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`;
+};
+
+const greenDiv = clicked.bind(null, 'div', 'green');
+const greenP = clicked.bind(null, 'p', 'green');
+const greenSpan = clicked.bind(null, 'span', 'green');
+divEl.addEventListener('click', greenDiv);
+pEl.addEventListener('click', greenP);
+spanEl.addEventListener('click', greenSpan);
+
+const greyDiv = clicked.bind(null, 'div', 'grey');
+const greyP = clicked.bind(null, 'p', 'grey');
+const greySpan = clicked.bind(null, 'span', 'grey');
+divEl.addEventListener('click', greyDiv, true);
+pEl.addEventListener('click', greyP, true);
+spanEl.addEventListener('click', greySpan, true);
