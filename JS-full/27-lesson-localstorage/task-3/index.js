a4b9789d9665ba79ebdbe835f7a71a4c8d09b27d
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-05-22 23:37:43 +0300

    m

--- JS-full/27-lesson-localstorage/task-3/index.js
+++ JS-full/27-lesson-localstorage/task-3/index.js
@@ -1,25 +1,60 @@
-const minutes = 60 * 1000;
-const hour = 60 * minutes;
-const day = 24 * hour;
+const tasks = [];
 
-const getDiff = (startDate, endDate) => {
-  const diffDay = Math.abs(endDate - startDate);
-  const countDays = Math.trunc(diffDay / day);
-  const countDaysInMs = countDays * day;
-
-  const DayInMs = diffDay - countDaysInMs;
-
-  const countHours = Math.trunc(DayInMs / hour);
-  const restHourInMs = DayInMs - countHours * hour;
-
-  const countMinutes = Math.trunc(restHourInMs / minutes);
-  const countSeconds = Math.trunc(
-    (restHourInMs - countMinutes * minutes) / 1000
-  );
-
-  return `${countDays}d ${countHours}h ${countMinutes}m ${countSeconds}s`;
+const listElem = document.querySelector('.list');
+const inputEl = document.querySelector('.task-input');
+const createButton = document.querySelector('.create-task-btn');
+const setItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
 };
-const startDate = new Date(2023, 5, 6, 9, 47, 3);
-const endDate = new Date(2022, 5, 10, 9, 47, 3);
-console.log(getDiff(startDate, endDate));
-export { getDiff };
+const getItem = (key) => {
+  JSON.parse(localStorage.getItem(key));
+};
+const renderTasks = () => {
+  const getTasksList = getItem("tasksList" || []);
+  listElem.innerHTML = '';
+  const tasksElems = getTasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.setAttribute('data-id', `${done}`);
+      const onClickCheckbox = () => {
+        checkbox.checked
+          ? listItemElem.classList.add('list__item_done')
+          : listItemElem.classList.remove('list__item_done');
+      };
+      checkbox.addEventListener('click', onClickCheckbox);
+
+      checkbox.classList.add('list__item-checkbox');
+
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
+
+const handleAddTask = (tasksList) => {
+  const newTasks = {
+    text: inputEl.value,
+    done: false,
+  };
+
+  tasks.push(newTasks);
+  tasks.filter((el) => {
+    if (el.text === inputEl.value && el.text.length != '') {
+      renderTasks([el]);
+    }
+  });
+};
+
+createButton.addEventListener('click', handleAddTask);
+
+renderTasks();
