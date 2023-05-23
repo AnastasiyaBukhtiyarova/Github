fb0fcec66b68ae221f438d4c500c976f68406a78
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-05-23 21:56:37 +0300

    localstorage

--- JS-full/27-lesson-localstorage/task-3/scripts/events.js
+++ JS-full/27-lesson-localstorage/task-3/scripts/events.js
@@ -22,5 +22,13 @@ tasks.filter((el) => {
   }
 });
 createButton.addEventListener('click', handleAddTask);
+Document.addEventListener('DOMContentLoaded', () => {
+  renderTasks(tasks);
+});
+
+const onStorageChange = (e) => {
+  if (e.key === 'tasks') renderTasks(tasks);
+};
+window.addEventListener('storage', onStorageChange);
 renderTasks(tasks);
 export { handleAddTask, tasks, renderTasks };
--- JS-full/27-lesson-localstorage/task-3/scripts/index.js
+++ JS-full/27-lesson-localstorage/task-3/scripts/index.js
@@ -1,7 +1,64 @@
-import { renderTasks } from './events.js';
+const listElem = document.querySelector('.list');
+const inputEl = document.querySelector('.task-input');
+const createButton = document.querySelector('.create-task-btn');
 
-import { tasks } from './events.js';
+const setItem = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+const getItem = (key) => JSON.parse(localStorage.getItem(key));
+const tasks = getItem('tasks') || [];
+const renderTasks = (tasksList) => {
+  const tasksElems = tasksList
+    .sort((a, b) => a.done - b.done)
+    .map(({ text, done, id }) => {
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+      const checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      checkbox.checked = done;
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+      listItemElem.setAttribute('data-id', `${done}`);
+      const onClickCheckbox = () => {
+        checkbox.checked
+          ? listItemElem.classList.add('list__item_done')
+          : listItemElem.classList.remove('list__item_done');
+      };
+      checkbox.addEventListener('click', onClickCheckbox);
 
-Document.addEventListener('DOMContentLoaded', () => {
-  renderTasks(tasks);
+      checkbox.classList.add('list__item-checkbox');
+
+      listItemElem.append(checkbox, text);
+
+      return listItemElem;
+    });
+
+  listElem.append(...tasksElems);
+};
+
+const handleAddTask = (tasksList) => {
+  const newTasks = {
+    text: inputEl.value,
+    done: false,
+  };
+
+  tasks.push(newTasks);
+  tasks.filter((el) => {
+    if (el.text === inputEl.value && el.text.length != '') {
+      console.log(el.text.length);
+      renderTasks([el]);
+    }
+  });
+};
+document.addEventListener('DOMContentLOader', () => {
+  renderTasks();
 });
+
+const onStorageChange = (e) => {
+  if (e.key === 'tasks') renderTasks();
+};
+window.addEventListener('storage', onStorageChange);
+createButton.addEventListener('click', handleAddTask);
+
+renderTasks(tasks);
