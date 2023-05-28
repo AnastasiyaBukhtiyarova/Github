a326528d047053940e7d7a38e3c701e17d3b96c6
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-05-28 22:31:56 +0300

    storage

--- JS-full/27-lesson-localstorage/task-3/scripts/toDoList.js
+++ JS-full/27-lesson-localstorage/task-3/scripts/toDoList.js
@@ -5,7 +5,6 @@ import { onToggleTask } from './updateTask.js';
 export const initToDoListHandlers = () => {
   const createBtnElem = document.querySelector('.create-task-btn');
   createBtnElem.addEventListener('click', onCreateTask);
-
-  const toDoListElem = document.querySelector('.list');
-  toDoListElem.addEventListener('click', onToggleTask);
 };
+const toDoListElem = document.querySelector('.list');
+toDoListElem.addEventListener('click', onToggleTask);
