f788cbcfeb51ab652135b5cb33db1f7f808adb15
AB <anabuhtiyarova97@gmail.com>
2023-05-31 12:20:36 +0300

    todolist+localstorage

---
+++ JS-full/27-lesson-localstorage/task-6-alternative-code/scripts/eventOnClick.js
@@ -0,0 +1,39 @@
+
+import { setItem,getItem } from './storage.js';
+
+import { renderListItems } from './render.js';
+
+export const onAddClick = function () {
+  const taskName = document.getElementById('task-name');
+
+  let tasks = getItem('tasks') || [];
+
+  if (taskName.value == null || taskName.value.trim() == '') {
+    return;
+  }
+
+  const task = {
+    text: taskName.value,
+    done: false,
+    createDate: Date.now(),
+  };
+
+  tasks.push(task);
+  setItem('tasks', tasks);
+
+  renderListItems();
+
+  taskName.value = '';
+};
+
+export function onDoneClick() {
+  const id = this.id;
+  let tasks = getItem('tasks') || [];
+  const task = tasks.find((t) => t.createDate === id);
+  task.done = this.checked;
+  task.createDate = Date.now();
+  setItem('tasks', tasks);
+  renderListItems();
+}
+const button = document.getElementById('add-btn');
+button.onclick = onAddClick();
