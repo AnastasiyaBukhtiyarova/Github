3f1df0cf1d1fa2c9917bd65116df64c4b85dc6f3
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-06-01 21:55:06 +0300

    func chaning

--- JS-full/28-lesson-functions-advanced/task-1/index.js
+++ JS-full/28-lesson-functions-advanced/task-1/index.js
@@ -1,30 +1,30 @@
 const calc = (initValue) => {
-  let mathResult = initValue;
+  let result = initValue;
 
   const mathOperations = {
     add(value) {
-      mathResult += value;
+      result += value;
       return this;
     },
 
     substract(value) {
-      mathResult -= value;
+      result -= value;
       return this;
     },
     multiply(value) {
-      mathResult *= value;
+     result *= value;
       return this;
     },
     divide(value) {
-      mathResult /= value;
+      result /= value;
       return this;
     },
-    mathResult() {
-      return mathResult;
+    result() {
+      return result;
     },
   };
   return mathOperations;
 };
-const result = calc(2).add(2).substract(2).divide(10).multiply(0).mathResult();
+const result = calc(2).add(2).substract(2).divide(10).multiply(0).result();
 console.log(result);
-export { calc };
+//export { calc };
