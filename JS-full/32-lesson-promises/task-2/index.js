343f8df45af4f4321b3c857e57a70868749adaa7
AB <anabuhtiyarova97@gmail.com>
2023-06-14 12:31:15 +0300

    promises

--- JS-full/32-lesson-promises/task-2/index.js
+++ JS-full/32-lesson-promises/task-2/index.js
@@ -1,24 +1,28 @@
-//export
-  const asyncCalculator = (number) =>
+const getRandomNumber = (from, to) => from + Math.random() * (to - from);
+const request = (url) =>
   new Promise((resolve) => {
+    const randomDelay = getRandomNumber(1000, 3000);
     setTimeout(() => {
-      console.log(`Initial value: ${number}`);
-      resolve(number);
-    }, 500);
-  })
-    .then(
-      (value) =>
-        new Promise((resolve) => {
-          setTimeout(() => {
-            const result = value * value;
-            console.log(`Squared value: ${result}`);
-            resolve(result);
-          }, 500);
-        })
-    )
-    .then((value) => {
-      const result = value * 2;
-      console.log(`Doubled value: ${result}`);
-      return result;
-    });
-asyncCalculator(4).then((value) => console.log(value));
+      resolve({
+        userData: {
+          name: 'Tom',
+          age: 17,
+        },
+        source: url,
+      });
+    }, randomDelay);
+  });
+
+const servers = [
+  'https://server.com/us',
+  'https://server.com/eu',
+  'https://server.com/au',
+];
+export const getUserASAP = (userId) => {
+  const userUrls = servers.map((serverUrl) => `${serverUrl}/users/${userId}`);
+
+  const requests = userUrls.map((userUrl) => request(userUrl));
+
+  return Promise.race(requests);
+};
+getUserASAP('user-id-1').then((res) => console.log(res));
--- JS-full/32-lesson-promises/task-3/index.js
+++ JS-full/32-lesson-promises/task-3/index.js
@@ -1,81 +1,43 @@
-const successPromise = new Promise((resolve) => {
-  resolve(32);
-});
-
 /*
- * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
  */
 
-successPromise
-  .then((number) => {
-    const halfNumber = number / 2;
-    return halfNumber;
-  })
-  .then((number) => {
-    /* fix this handler */
-    const squaredNumber = number * number;
-    return squaredNumber;
-  })
-  .then((result) => {
-    console.log(result); // 256
+const makePromise = (number) => {
+  const p = new Promise((resolve) => {
+    resolve(number);
   });
-
-/*
- * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
- */
-successPromise
-  .then((number) => {
-    /* fix this handler */
-    return number * 10;
-  })
-  .then((result) => {
-    console.log(result); // 320
-  });
-
-console.log(
-  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!'
-);
-
-// part 2
-
-const serverResponsePromise = new Promise((resolve) => {
-  const serverResponse = {
-    ok: true,
-    json() {
-      return Promise.resolve({
-        name: 'John',
-        age: 20,
-      });
-    },
-  };
-  resolve(serverResponse);
-});
-
-/*
- * допиши первый обработчик, чтобы во второй попал объект пользователя
- */
-
-serverResponsePromise
-  .then((response) => {
-    /* ...code here */
-    return response.json();
-  })
-  .then((result) => {
-    console.log(result); // { name: 'John', age: 20 }
-  });
-
-console.log(
-  '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!'
-);
-
-//part 3
-
-const delay = (time) => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('Done');
-    }, time);
-  });
-  return promise;
+  return p;
 };
-delay(3000).then((data) => console.log(data));
+/*
+ * пример использования
+ */
+makePromise(17).then((number) => {
+  console.log(number); // 17
+});
+//part 2
+
+const promiseNumber1 = 67;
+const promiseNumber2 = 23;
+const promiseNumber3 = 8;
+
+/*
+ * создай промис и присвой переменной resultPromise
+ * чтобы в консоль вывелась сумма всех чисел из трех промисов
+ */
+
+// update code below
+
+// export
+const resultPromise = new Promise((resolve) => {
+  resolve([promiseNumber1, promiseNumber2, promiseNumber3]);
+});
+
+resultPromise
+  .then((numbersList) => {
+    console.log(numbersList);
+    const sum = numbersList.reduce((acc, num) => acc + num, 0);
+    return sum;
+  })
+  .then((result) => {
+    console.log(result); // 98
+  });
