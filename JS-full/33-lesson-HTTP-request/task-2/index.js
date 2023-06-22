739d8ec5cdfd068846278267914f1f840846d6bf
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-06-22 09:56:10 +0300

    HTTP

--- JS-full/33-lesson-HTTP-request/task-2/index.js
+++ JS-full/33-lesson-HTTP-request/task-2/index.js
@@ -1,28 +1,26 @@
-const getRandomNumber = (from, to) => from + Math.random() * (to - from);
-const request = (url) =>
-  new Promise((resolve) => {
-    const randomDelay = getRandomNumber(1000, 3000);
-    setTimeout(() => {
-      resolve({
-        userData: {
-          name: 'Tom',
-          age: 17,
-        },
-        source: url,
-      });
-    }, randomDelay);
-  });
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
-const servers = [
-  'https://server.com/us',
-  'https://server.com/eu',
-  'https://server.com/au',
-];
-export const getUserASAP = (userId) => {
-  const userUrls = servers.map((serverUrl) => `${serverUrl}/users/${userId}`);
+function getTasksList() {
+  // put your code here
+  fetch(baseUrl)
+    .then((baseUrl) => baseUrl.json())
+    .then((baseUrl) => console.log(baseUrl));
+}
 
-  const requests = userUrls.map((userUrl) => request(userUrl));
+export const getTaskById = (taskId) =>
+  fetch(`${baseUrl}/${taskId}`).then((response) => response.json());
 
-  return Promise.race(requests);
-};
-getUserASAP('user-id-1').then((res) => console.log(res));
+/* first option */
+
+export const getTaskById1 = (taskId) =>
+  getTasksList().then((userData) => userData.find((el) => el.id === taskId));
+/* second option */
+
+// examples
+getTasksList().then((tasksList) => {
+  console.log(tasksList); // ==> [ {'id':'1', 'isDone':false ... }, {'id':'2', 'isDone':false ... }, ...]
+});
+
+getTaskById('2').then((taskData) => {
+  console.log(taskData); // ==> { 'id': '2', 'text': 'District Communications Specialist', 'isDone': false, 'createdDate': 1651499052, 'finishedDate': 1651499052 }
+});
