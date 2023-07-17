caa4b08f0f9c0bae7cbaac3ceef054cff95edab1
AB <anabuhtiyarova97@gmail.com>
2023-07-17 14:07:52 +0300

    create user form

--- JS-full/34-lesson-HTTP-practise/task-1/index.js
+++ JS-full/34-lesson-HTTP-practise/task-1/index.js
@@ -2,16 +2,23 @@ const baseUrl = 'https://64abe8419edb4181202ec69a.mockapi.io/login-form/user';
 const registerButton = document.querySelector('.submit-button');
 const loginForm = document.querySelector('.login-form');
 
-const postUser = (data) => {
+const postUser = (event) => {
+  event.preventDefault();
+  const formData = Object.fromEntries(new FormData(loginForm));
   return fetch(baseUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
-  }).then((response) => {
-    response.json();
-  });
+    body: JSON.stringify(formData),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      alert(JSON.stringify(data));
+      loginForm.reset();
+    });
 };
 const onUserValidity = () => {
   if (loginForm.reportValidity() === true) {
@@ -19,18 +26,8 @@ const onUserValidity = () => {
   } else {
     registerButton.setAttribute('disabled', true);
   }
-  //console.log(loginForm.reportValidity());
-};
-const onCreateUser = () => {
-  const formData = Object.fromEntries(new FormData(loginForm));
-  postUser(formData);
-  alert(JSON.stringify(formData));
 };
 
-const onResetForm = (event) => {
-  event.preventDefault();
-  event.reset();
-};
+const onResetForm = (event) => {};
 loginForm.addEventListener('input', onUserValidity);
-registerButton.addEventListener('click', onCreateUser);
-registerButton.addEventListener('reset', onResetForm);
+registerButton.addEventListener('click', postUser);
