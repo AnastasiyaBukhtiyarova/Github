11afcc0b0d4166ea2dced080cc48c5b4e307ba85
AB <anabuhtiyarova97@gmail.com>
2023-07-18 14:09:55 +0300

    crud requests

--- JS-full/34-lesson-HTTP-practise/task-2/index.js
+++ JS-full/34-lesson-HTTP-practise/task-2/index.js
@@ -1,30 +1,44 @@
-const baseUrl = 'https://64b53125f3dbab5a95c6e5d0.mockapi.io/resourse/';
-function getUsersList() {
-  // put your code here
+const baseUrl = 'https://64b53125f3dbab5a95c6e5d0.mockapi.io/resourse';
+
+export function getUsersList() {
   return fetch(baseUrl).then((response) => response.json());
 }
 
 export function getUserById(userId) {
-  // put your code here
-  const user = `${baseUrl}/${userId}`;
-  getUsersList.filter((data) => data.includes(user));
-  //  console.log(user);
-  return user;
+  return fetch(`${baseUrl}/${userId}`).then((response) => response.json());
 }
 
 export function createUser(userData) {
-  // put your code here
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 }
 
 export function deleteUser(userId) {
   // put your code here
+
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
 
 export function updateUser(userId, userData) {
   // put your code here
+  return fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  });
 }
-
-// examples
 getUsersList().then((users) => {
   console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
 });
@@ -32,7 +46,6 @@ getUsersList().then((users) => {
 getUserById('2').then((userData) => {
   console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
 });
-
 const newUserData = {
   email: 'cool@email.com',
   firstName: 'Iron',
