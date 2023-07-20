998053119a27780f88be87d51f3935e21ac954a1
Stacy Bukhtiyarova <stacywdeveloper@gmail.com>
2023-07-20 23:48:36 +0300

    .

--- JS-full/34-lesson-HTTP-practise/task-2/index.js
+++ JS-full/34-lesson-HTTP-practise/task-2/index.js
@@ -1,70 +1,23 @@
-const baseUrl = 'https://64b53125f3dbab5a95c6e5d0.mockapi.io/users';
-
-export function getUsersList() {
-  return fetch(baseUrl).then((response) => response.json());
-}
-
-export function getUserById(userId) {
-  return fetch(`${baseUrl}/${userId}`).then((response) => response.json());
-}
-
-export function createUser(userData) {
-  return fetch(baseUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
+const successRequest = Promise.resolve({ name: 'Tom' });
+successRequest
+  .then(function onSuccess1(data) {
+    // (1)
+    throw new Error('Error with data');
+  })
+  .catch(function onError1(error) {
+    console.error('onError1', error.message);
   });
-}
 
-export function deleteUser(userId) {
-  // put your code here
+const failRequest = Promise.reject(new Error('Something went wrong'));
 
-  return fetch(`${baseUrl}/${userId}`, {
-    method: 'DELETE',
+failRequest
+  .catch(function onError2(error) {
+    throw new Error('Server error');
+    // (2)
+  })
+  .then(function onSuccess2(data) {
+    console.log('onSuccess2', data);
+  })
+  .catch(function onError3(error) {
+    console.error('onError3', error.message);
   });
-}
-
-export function updateUser(userId, userData) {
-  // put your code here
-  return fetch(`${baseUrl}/${userId}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
-}
-getUsersList().then((users) => {
-  console.log(users); // array of the user objects [{'id':'1', 'firstName':'Grayce' ... }, {'id':'2', 'firstName':'Ara' ... }, ...]
-});
-
-getUserById('2').then((userData) => {
-  console.log(userData); // {'id':'2', 'firstName':'Ara' ... }
-});
-const newUserData = {
-  email: 'cool@email.com',
-  firstName: 'Iron',
-  lastName: 'Man',
-  age: 42,
-};
-
-createUser(newUserData).then(() => {
-  console.log('User created');
-});
-
-const updatedUserData = {
-  email: 'new@email.com',
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 17,
-};
-
-updateUser('1', updatedUserData).then(() => {
-  console.log('User updated');
-});
-
-deleteUser('2').then(() => {
-  console.log('User updated');
-});
