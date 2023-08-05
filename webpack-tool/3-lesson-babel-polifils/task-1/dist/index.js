b5fb97b1012dced1b0d41d34cd9204ca8e2825c7
Anastasiya <abdeveloperit@gmail.com>
2023-08-05 11:10:50 +0300

    .

--- webpack-tool/3-lesson-babel-polifils/task-1/dist/index.js
+++ webpack-tool/3-lesson-babel-polifils/task-1/dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
 
-const data = [1, 2, [(3, 4, [5, 6])]];
-const result = data.flat(2);
+require("core-js/modules/es7.promise.finally.js");
+require("core-js/modules/es6.object.to-string.js");
+require("core-js/modules/es6.promise.js");
+var data = [1, 2, [(3, 4, [5, 6])]];
+var result = data.flat(2);
 console.log(result);
 Promise.resolve().finally();
