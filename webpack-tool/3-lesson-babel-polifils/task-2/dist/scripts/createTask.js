0ca45bf65dce43af022465807783e79d7e5f3378
Anastasiya <abdeveloperit@gmail.com>
2023-08-06 16:13:27 +0300

    babel polyfils

--- webpack-tool/3-lesson-babel-polifils/task-2/dist/scripts/createTask.js
+++ webpack-tool/3-lesson-babel-polifils/task-2/dist/scripts/createTask.js
@@ -1,51 +1,46 @@
+import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.regexp.to-string.js";
 import { renderTasks } from './renderer.js';
 import { getItem, setItem } from './storage.js';
 import { createTask } from './serverExchange.js';
 import { getTasksList } from './serverExchange.js';
-
-export const onCreateTask = () => {
-  const taskTitleInputElem = document.querySelector('.task-input');
-
-  const text = taskTitleInputElem.value;
+export var onCreateTask = function onCreateTask() {
+  var taskTitleInputElem = document.querySelector('.task-input');
+  var text = taskTitleInputElem.value;
   if (!text) {
     return;
   }
   taskTitleInputElem.value = '';
-
-  const newTask = {
-    text,
+  var newTask = {
+    text: text,
     done: false,
     createDate: new Date().toISOString(),
-    id: Math.random().toString(),
+    id: Math.random().toString()
   };
-  createTask(newTask)
-    .then(() => getTasksList())
-    .then((newTasksList) => {
-      setItem('tasksList', newTasksList);
-      renderTasks();
-    });
+  createTask(newTask).then(function () {
+    return getTasksList();
+  }).then(function (newTasksList) {
+    setItem('tasksList', newTasksList);
+    renderTasks();
+  });
 };
-
-export const onDeleteTask = () => {
-  const taskTitleInputElem = document.querySelector('.task-input');
- 
-  const text = taskTitleInputElem.value;
+export var onDeleteTask = function onDeleteTask() {
+  var taskTitleInputElem = document.querySelector('.task-input');
+  var text = taskTitleInputElem.value;
   if (!text) {
     return;
   }
   taskTitleInputElem.value = '';
-
-  const newTask = {
-    text,
+  var newTask = {
+    text: text,
     done: false,
     createDate: new Date().toISOString(),
-     id: Math.random().toString(),
-    
+    id: Math.random().toString()
   };
-  createTask(newTask)
-    .then(() => getTasksList())
-    .then((newTasksList) => {
-      setItem('tasksList', newTasksList);
-      renderTasks();
-    });
+  createTask(newTask).then(function () {
+    return getTasksList();
+  }).then(function (newTasksList) {
+    setItem('tasksList', newTasksList);
+    renderTasks();
+  });
 };
--- webpack-tool/3-lesson-babel-polifils/task-2/dist/scripts/toDoList.js
+++ webpack-tool/3-lesson-babel-polifils/task-2/dist/scripts/toDoList.js
@@ -3,38 +3,29 @@ import { onToggleTask } from './updateTask.js';
 import { deleteTask, getTasksList } from './serverExchange.js';
 import { renderTasks } from './renderer.js';
 import { setItem, getItem } from './storage.js';
-
-const onDeleteTask = (e) => {
-  const taskId = e.target.dataset.id;
-
-  deleteTask(taskId)
-    .then(() => getTasksList())
-    .then((newTasksList) => {
-      setItem('tasksList', newTasksList);
-      renderTasks();
-    });
+var onDeleteTask = function onDeleteTask(e) {
+  var taskId = e.target.dataset.id;
+  deleteTask(taskId).then(function () {
+    return getTasksList();
+  }).then(function (newTasksList) {
+    setItem('tasksList', newTasksList);
+    renderTasks();
+  });
 };
-
-export const onListClick = (e) => {
-  const isCheckbox = e.target.classList.contains('list__item-checkbox');
+export var onListClick = function onListClick(e) {
+  var isCheckbox = e.target.classList.contains('list__item-checkbox');
   if (isCheckbox) {
     onToggleTask(e);
     return;
   }
-
-  const isDeleteButton = e.target.classList.contains('list__item-delete-btn');
-
+  var isDeleteButton = e.target.classList.contains('list__item-delete-btn');
   if (isDeleteButton) {
     onDeleteTask(e);
   }
 };
-
-export const initTodoListHandlers = () => {
-  const createBtnElem = document.querySelector('.create-task-btn');
-
+export var initTodoListHandlers = function initTodoListHandlers() {
+  var createBtnElem = document.querySelector('.create-task-btn');
   createBtnElem.addEventListener('click', onCreateTask);
-
-  const todoListElem = document.querySelector('.list');
-
+  var todoListElem = document.querySelector('.list');
   todoListElem.addEventListener('click', onListClick);
 };
