8aaca5b4fb194abc2ba4d9f276137579660361ae
Anastasiya <abdeveloperit@gmail.com>
2023-08-19 15:53:16 +0300

    .

---
+++ webpack-tool/7-lesson-plugins/task-1/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: ['./src/index.js', './src/profile/profile.scss', './src/index.css'],
+  output: {
+    filename: 'bundle.js',
+    assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        type: 'asset/resource',
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+            fileName: '[name].[ext]',
+            outputPath: 'images',
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+};
