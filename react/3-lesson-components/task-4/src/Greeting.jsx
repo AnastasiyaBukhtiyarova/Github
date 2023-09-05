9c85a76d1c02c2d3f13f6d4df317b2578f7cd2f0
Anastasiya <abdeveloperit@gmail.com>
2023-09-05 22:23:04 +0300

    .

--- react/3-lesson-components/task-4/src/Greeting.jsx
+++ react/3-lesson-components/task-4/src/Greeting.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import moment from 'moment';
 //const rootElement = document.querySelector('#root');
+
 const Greeting = (props) => {
+  console.log(props);
   return (
     <div className="greeting">
       My name is {props.firstName} {props.lastName}. I am {props.birthDate}{' '}
--- react/3-lesson-components/task-4/src/index.jsx
+++ react/3-lesson-components/task-4/src/index.jsx
@@ -4,15 +4,20 @@ import './index.scss';
 import Greeting from './Greeting';
 import moment from 'moment';
 const rootElement = document.querySelector('#root');
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  birthDate: moment('05092006', 'DDMMYYYY'),
+};
 ReactDOM.render(
   <Greeting
-    firstName={'John'}
+    /* firstName={'John'}
     lastName={'Doe'}
-    birthDate={-moment('05092006', 'DDMMYYYY').diff(
-      moment(),
-      'years'
-    )}
+    birthDate={-moment('05092006', 'DDMMYYYY').diff(moment(), 'years')}
+    */
+    firstName={user.firstName}
+    lastName={user.lastName}
+    birthDate={-user.birthDate.diff(moment(), 'years')}
   />,
   rootElement
 );
-
