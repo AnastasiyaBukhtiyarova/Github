1eb13092bf8f519b3e27b9889ea205ffec6a37c8
Anastasiya <abdeveloperit@gmail.com>
2023-09-20 20:24:43 +0300

    .

---
+++ react/6-lesson-conditional-rendering/task-2/src/Mailbox.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Mailbox = ({ UnreadMessanges }) => {
+  return (
+    <div className="mailbox">
+      <span className="mailbox__text">Messages</span>
+      {UnreadMessanges.length > 0 && (
+        <span className="mailbox__count">{UnreadMessanges.length}</span>
+      )}
+    </div>
+  );
+};
+
+export default Mailbox;
