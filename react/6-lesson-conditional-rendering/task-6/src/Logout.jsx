e024132425f0b70e989762407dd0b0021781e133
AB <anabuhtiyarova97@gmail.com>
2023-09-20 17:16:26 +0300

    .

--- react/6-lesson-conditional-rendering/task-6/src/Logout.jsx
+++ react/6-lesson-conditional-rendering/task-6/src/Logout.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import React, { Component } from 'react';
-import Auth from './Auth';
+
 
 const Logout = (props) => {
   return <button onClick={props.onLogout}>Login</button>;
