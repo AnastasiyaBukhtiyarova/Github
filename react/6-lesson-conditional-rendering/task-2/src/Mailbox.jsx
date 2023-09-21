b561467378be7cae8e12092cf77872b695810577
AB <anabuhtiyarova97@gmail.com>
2023-09-21 16:00:15 +0300

    .

--- react/6-lesson-conditional-rendering/task-2/src/Mailbox.jsx
+++ react/6-lesson-conditional-rendering/task-2/src/Mailbox.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Mailbox = ({ UnreadMessanges }) => {
+const Mailbox = ({ UnreadMessages }) => {
   return (
     <div className="mailbox">
       <span className="mailbox__text">Messages</span>
-      {UnreadMessanges.length > 0 && (
+      {UnreadMessages.length > 0 && (
         <span className="mailbox__count">{UnreadMessages.length}</span>
       )}
     </div>
