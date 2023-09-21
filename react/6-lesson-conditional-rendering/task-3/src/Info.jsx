b10c2e8bc0ca83f337831280e116f53f6e561246
AB <anabuhtiyarova97@gmail.com>
2023-09-21 18:09:59 +0300

    .

--- react/6-lesson-conditional-rendering/task-3/src/Info.jsx
+++ react/6-lesson-conditional-rendering/task-3/src/Info.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './index.scss';
 
 const Info = ({ info }) => {
+  if (!info) {
+    return null;
+  }
   return <div className="info">{info}</div>;
 };
 export default Info;
