4ba93d17136a22e2b6c14cd1eff0c85951840d3b
Anastasiya <abdeveloperit@gmail.com>
2023-09-25 21:24:15 +0300

    .

---
+++ react/6-lesson-conditional-rendering/task-5/src/Login.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Auth from './Auth';
+
+const Login = (props) => {
+  
+  return <button className="login btn">Login</button>;
+};
+
+export default Login;
---
+++ react/6-lesson-conditional-rendering/task-5/src/Spinner.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Auth from './Auth';
+
+const Spinner = (props) => {
+  return <span className="spinner"></span>;
+};
+
+export default Spinner;
