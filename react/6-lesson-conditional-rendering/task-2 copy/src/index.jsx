033df1c5ffdc7dd4221576d0b73d91c43411e463
Anastasiya <abdeveloperit@gmail.com>
2023-09-25 21:02:56 +0300

    .

---
+++ react/6-lesson-conditional-rendering/task-2 copy/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Mailbox from './Mailbox';
+
+const rootElement = document.querySelector('#root');
+ReactDOM.render(<Mailbox UnreadMessanges={[5]} />, rootElement);
