4a93c200c4a143ca7b36eef71b060fea6b54dcba
AB <anabuhtiyarova97@gmail.com>
2023-09-26 15:47:21 +0300

    .

--- react/7-lesson-lists/task-2/src/UserList.jsx
+++ react/7-lesson-lists/task-2/src/UserList.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-const UserList = ({ users }) => {
-  return (
-    {
-      users.map(user => <ul>
-        <li>
-          <span></span>
-      </li>
-    </ul>)}
-  )
-};
+import React, { Component } from 'react';
+import User from './User';
+class UserList extends Component {
+  state = {
+    sorting: null,
+  };
+  toggleSorting = () => {
+    const newSorting = this.state.sorting === 'asc' ? 'desc' : 'asc';
+    this.setState({
+      sorting: newSorting,
+    });
+  };
+  render() {
+    let usersList;
+    if (this.state.sorting) {
+      usersList = this.props.users
+        .slice()
+        .sort((a, b) =>
+          this.state.sorting === 'asc' ? a.age - b.age : b.age - a.age
+        );
+    } else {
+      usersList = this.props.users;
+    }
+
+    return (
+      <>
+        <div>
+          <button className="btn" onClick={this.toggleSorting}>
+            {this.state.sorting || '-'}
+          </button>
+        </div>
+        <ul className="users">
+          {this.props.users.map((user) => (
+            <User name={user.name} {...user} />
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
 export default UserList;
