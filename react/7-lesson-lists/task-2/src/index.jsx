a68a21a2b30968ee390d621559327afbb2ea42af
AB <anabuhtiyarova97@gmail.com>
2023-09-26 16:08:22 +0300

    .

--- react/7-lesson-lists/task-2/src/index.jsx
+++ react/7-lesson-lists/task-2/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import UserList from './UserList';
+import UsersList from './UserList';
 const rootElement = document.querySelector('#root');
 const users = [
   {
@@ -26,4 +26,4 @@ const users = [
   },
 ];
 
-ReactDOM.render(<UserList users={ users} />, rootElement);
+ReactDOM.render(<UsersList users={users} />, rootElement);
