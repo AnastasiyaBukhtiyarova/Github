9fd118dc328dc6d35a7f16ffae2ea20544f1b9f7
Anastasiya <abdeveloperit@gmail.com>
2023-09-28 21:47:14 +0300

    .

--- react/8-lesson-lifecycle-methods/task-1/src/Life.jsx
+++ react/8-lesson-lifecycle-methods/task-1/src/Life.jsx
@@ -6,20 +6,22 @@ class Life extends Component {
     console.log('constructor: good place to create state');
   }
   componentDidMount() {
-    console.log('componentDidMount');
+    console.log('componentDidMount: API calls, subscriptions');
   }
   shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate');
+    console.log('shouldComponentUpdate(nextProps, nextState)');
     return nextProps.number % 2;
   }
   componentDidUpdate(previousProps, previousState) {
-    console.log('componentDidUpdate');
+    console.log('componentDidUpdate(prevProps, prevState)');
   }
   componentWillUnmount() {
-    console.log('componentWillUnmount');
+    console.log(
+      'componentWillUnmount(): cleanup before DOM related to component will be removed'
+    );
   }
   render() {
-    console.log('render');
+    console.log('return React element to build DOM');
     return <div className="number">{this.props.number}</div>;
   }
 }
