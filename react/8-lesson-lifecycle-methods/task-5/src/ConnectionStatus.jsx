5d0c82a91383e9888a0b5643c1581a1f6ce0104f
AB <anabuhtiyarova97@gmail.com>
2023-10-05 13:13:20 +0300

    /

--- react/8-lesson-lifecycle-methods/task-5/src/ConnectionStatus.jsx
+++ react/8-lesson-lifecycle-methods/task-5/src/ConnectionStatus.jsx
@@ -19,16 +19,8 @@ class ConnectionStatus extends Component {
     });
   }
   componentDidMount() {
-    window.removeEventListener('online', () => {
-      this.setState({
-        status: true,
-      });
-    });
-    window.removeEventListener('offline', () => {
-      this.setState({
-        status: false,
-      });
-    });
+    window.removeEventListener('online');
+    window.removeEventListener('offline');
   }
 
   render() {
