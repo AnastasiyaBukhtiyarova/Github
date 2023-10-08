e5517c77da04dbea76db2a257d9c9ee55d2490be
Stacy B <stacywdeveloper@gmail.com>
2023-10-08 17:18:29 +0300

    /

---
+++ react/9-lesson-forms/task-4/src/Filter.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Filter extends Component {
+  render() {
+    return (
+      <div className="filter">
+        <span className="filter__count">{this.props.count }</span>
+        <input
+          value={this.props.text}
+          onChange={this.props.onChange}
+          type="text"
+          className="filter__input"
+        />
+      </div>
+    );
+  }
+}
+
+export default Filter;
---
+++ react/9-lesson-forms/task-4/src/UsersList.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import User from './User';
+import Filter from './Filter';
+class UsersList extends Component {
+  state = {
+    value: '',
+    count: 0,
+    filteredUser: this.props.users,
+  };
+  handleInput = (event) => {
+    let users = this.props.users
+      .filter((user) => user.name.toLowerCase().match(event.target.value.toLowerCase()));
+    
+    console.log(users);
+
+    this.setState({
+      value: event.target.value,
+      filteredUser: users,
+      count: users.length
+    });
+  };
+  render() {
+    return (
+      <div>
+        <Filter
+          filterText={this.state.value}
+          onChange={this.handleInput}
+          count={this.state.count}
+        />
+
+        <ul className="users">
+          {this.state.filteredUser.map((user) => (
+            <User key={user.id} {...user} />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+export default UsersList;
