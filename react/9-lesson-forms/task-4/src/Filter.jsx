86ef47f38407964a425231980eac815f7d5b0cc5
AB <anabuhtiyarova97@gmail.com>
2023-10-09 15:17:45 +0300

    /

--- react/9-lesson-forms/task-4/src/Filter.jsx
+++ react/9-lesson-forms/task-4/src/Filter.jsx
@@ -4,8 +4,9 @@ class Filter extends Component {
   render() {
     return (
       <div className="filter">
-        <span className="filter__count">{this.props.count }</span>
+        <span className="filter__count">{this.props.count}</span>
         <input
+          filterText={this.props.filterText}
           value={this.props.text}
           onChange={this.props.onChange}
           type="text"
