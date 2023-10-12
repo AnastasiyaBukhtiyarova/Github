0b8dfd32588294295d41d0fe85535d96f8b66be2
AB <anabuhtiyarova97@gmail.com>
2023-10-12 16:36:52 +0300

    .

--- react/10-lesson-lifting-state/task-2/src/CartTitle.jsx
+++ react/10-lesson-lifting-state/task-2/src/CartTitle.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 const CartTitle = ({ userName, count }) => {
-  return (
-    <div>{`${ userName }, you added ${count} products`}</div>
-  );
+  return <div>{`${userName}, you added ${count} items`}</div>;
 };
 export default CartTitle;
