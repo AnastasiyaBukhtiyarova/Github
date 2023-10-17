d0686925069be2f9a484de15a64a55272e4af00a
Stacy B <stacywdeveloper@gmail.com>
2023-10-18 01:36:16 +0300

    .

--- react/11-lesson-components-change/task-3/src/App.jsx
+++ react/11-lesson-components-change/task-3/src/App.jsx
@@ -4,33 +4,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import Expand from './Expand';
 class App extends Component {
-  state = {
-    isOpen: false,
-  };
-  hideDialog = () => {
-    this.setState({
-      isOpen: false,
-    });
-  };
-  showDialog = () => {
-    this.setState({
-      isOpen: true,
-    });
-  };
   render() {
     return (
       <div className="app">
-        <Expand
-          isOpen={this.state.isOpen}
-          onClose={this.hideDialog}
-          onOpen={this.showDialog}
-          title="some title">
-          {
-            <p>
-              Hooks are a new addition in React 16.8. They let you use state and
-              other React features without writing a class.
-            </p>
-          }
+        <Expand title="Some title">
+          <p>
+            Hooks are a new addition in React 16.8. They let you use state and
+            other React features without writing a class.
+          </p>
         </Expand>
       </div>
     );
--- react/11-lesson-components-change/task-3/src/Expand.jsx
+++ react/11-lesson-components-change/task-3/src/Expand.jsx
@@ -1,37 +1,64 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+library.add(faChevronUp, faChevronDown);
+class Expand extends Component {
+  state = {
+    hiddenChildren: false,
+  };
+  hideChildren = () => {
+    this.setState({
+      hiddenChildren: false,
+    });
+  };
+  showChildren = () => {
+    this.setState({
+      hiddenChildren: true,
+    });
+  };
+  render() {
+    const { title, children } = this.props;
 
-library.add(faChevronUp);
-const Expand = ({ isOpen, children, title, onClose, onOpen }) => {
-  if (isOpen) {
-    return <FontAwesomeIcon icon="chevron-up" />;
-  }
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button
-          className="expand__toggle-btn"
-          onClick={onClose}
-          onClick={onOpen}>
-          <FontAwesomeIcon icon="chevron-up" />
-        </button>
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+
+          {this.state.hiddenChildren ? (
+            <button className="expand__toggle-btn">
+              {' '}
+              <FontAwesomeIcon icon="chevron-up" onClick={this.hideChildren} />
+            </button>
+          ) : (
+            <button className="expand__toggle-btn">
+              <FontAwesomeIcon
+                icon="chevron-down"
+                onClick={this.showChildren}
+              />
+            </button>
+          )}
+        </div>
+
+        {this.state.hiddenChildren ? (
+          <div className="expand__content">{children}</div>
+        ) : null}
       </div>
-      <div className="expand__content">{children}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-/*Dialog.PropTypes = {
-  isOpen: PropTypes.bool,
-  children: PropTypes.element.isRequired,
-  title: PropTypes.string,
-  onClose: PropTypes.func.isRequired,
-};
-Dialog.defaultProps = {
-  isOpen: false,
-  title: '',
-}; */
 export default Expand;
+/*<button className="expand__toggle-btn" onClick={this.showChildren}>
+            {this.state.hiddenChildren ? (
+              <FontAwesomeIcon icon="chevron-up" onClick={this.hideChildren} />
+            ) : (
+              <FontAwesomeIcon
+                icon="chevron-down"
+                onClick={this.showChildren}
+              />
+            )}
+          </button>
+        </div>
+*/
