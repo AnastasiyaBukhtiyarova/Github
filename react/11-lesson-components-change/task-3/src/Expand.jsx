42eda0499724d2ea025f906815fac04da89307b5
Stacy B <stacywdeveloper@gmail.com>
2023-10-18 02:04:02 +0300

    .

--- react/11-lesson-components-change/task-3/src/Expand.jsx
+++ react/11-lesson-components-change/task-3/src/Expand.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+/*import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 library.add(faChevronUp, faChevronDown);
+*/
 class Expand extends Component {
   state = {
     hiddenChildren: false,
@@ -22,9 +23,9 @@ class Expand extends Component {
 
           <button className="expand__toggle-btn" onClick={this.toggleButton}>
             {this.state.hiddenChildren ? (
-              <FontAwesomeIcon icon="chevron-up" />
+              <i class="fas fa-chevron-up" />
             ) : (
-              <FontAwesomeIcon icon="chevron-down" />
+              <i class="fas fa-chevron-down" />
             )}
           </button>
         </div>
