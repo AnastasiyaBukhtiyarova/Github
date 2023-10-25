672987beccbe2f47fda5b0d6002cd51bc3bcca94
Stacy B <stacywdeveloper@gmail.com>
2023-10-25 13:13:01 +0300

    .

--- react/13-lesson-react-router/task-1/src/Products.jsx
+++ react/13-lesson-react-router/task-1/src/Products.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 const Products = () => {
   return (
     <div className="page__content">
       <h1>Products</h1>
       <p>We will add some products. Later... 😇</p>
-      <a href="/contacts">Contacts</a>
+      <Link href="/contacts">Contacts</Link>
     </div>
   );
 };
