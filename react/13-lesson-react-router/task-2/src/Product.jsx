9ea4a8c9e57a9a91902a3851524cb999cad6071e
Stacy B <stacywdeveloper@gmail.com>
2023-10-29 20:20:38 +0200

    .

--- react/13-lesson-react-router/task-2/src/Product.jsx
+++ react/13-lesson-react-router/task-2/src/Product.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 const Product = () => {
   const { productId } = useParams();
-  return <div className="product">{`Product is a ${productId}`}</div>;
+  return <div className="product">{`${productId}`}</div>;
 };
 export default Product;
