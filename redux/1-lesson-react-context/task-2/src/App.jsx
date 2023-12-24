9db461ad1cb2ca53032851e7e3368f7f0a8ff7f6
Stacy B <stacywdeveloper@gmail.com>
2023-12-24 22:44:19 +0200

    moved userData to App

--- redux/1-lesson-react-context/task-2/src/App.jsx
+++ redux/1-lesson-react-context/task-2/src/App.jsx
@@ -3,12 +3,15 @@ import Header from './Header';
 import { userData, Users } from './context.js';
 class App extends Component {
   state = {
-    user: userData,
+    userData: {
+      name: 'Nikola Tesla',
+      avatarUrl: 'https://avatars3.githubusercontent.com/u10001',
+    },
   };
 
   render() {
     return (
-      <Users.Provider value={this.state.user}>
+      <Users.Provider value={this.state.userData}>
         <div className="page">
           <Header></Header>
         </div>
--- redux/1-lesson-react-context/task-2/src/context.js
+++ redux/1-lesson-react-context/task-2/src/context.js
@@ -1,6 +1,3 @@
 import React from 'react';
-export const userData = {
-  name: 'Nikola Tesla',
-  avatarUrl: 'https://avatars3.githubusercontent.com/u10001',
-};
+
 export const Users = React.createContext({});
