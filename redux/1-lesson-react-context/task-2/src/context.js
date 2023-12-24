741b10e898ad33374971b708fc99b554ad15a85f
Stacy B <stacywdeveloper@gmail.com>
2023-12-24 15:31:34 +0200

    react context

--- redux/1-lesson-react-context/task-2/src/context.js
+++ redux/1-lesson-react-context/task-2/src/context.js
@@ -1,3 +1,7 @@
 import React from 'react';
+const context = {
+  name: '',
+  avatar_url: '',
+};
 
-export const Users = React.createContext({});
+export const Users = React.createContext(context);
