affed326844ee4271ab749abac31c561dc3789c9
Stacy B <stacywdeveloper@gmail.com>
2024-01-01 22:21:48 +0200

    redux devtools

---
+++ redux/3-lesson-combining-reducers/task-2/src/counter.js
@@ -0,0 +1,56 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+const initialState = {
+  value: 0,
+  history: [],
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        value: state.value + 1,
+        history: state.history.concat('+1'),
+      };
+    case DECREMENT:
+      return {
+        value: state.value - 1,
+        history: state.history.concat('-1'),
+      };
+    case RESET:
+      return {
+        value: 0,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export default store;
