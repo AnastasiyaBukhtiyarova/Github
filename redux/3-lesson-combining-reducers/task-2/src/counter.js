35223f2862fddcd9976b8bae2dd6f74a3d4b8b67
Stacy B <stacywdeveloper@gmail.com>
2024-01-01 22:25:52 +0200

    redux

--- redux/3-lesson-combining-reducers/task-2/src/counter.js
+++ redux/3-lesson-combining-reducers/task-2/src/counter.js
@@ -30,20 +30,11 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return {
-        value: state.value + 1,
-        history: state.history.concat('+1'),
-      };
+      return state + 1;
     case DECREMENT:
-      return {
-        value: state.value - 1,
-        history: state.history.concat('-1'),
-      };
+      return state - 1;
     case RESET:
-      return {
-        value: 0,
-        history: [],
-      };
+      return (state = 0);
     default:
       return state;
   }
