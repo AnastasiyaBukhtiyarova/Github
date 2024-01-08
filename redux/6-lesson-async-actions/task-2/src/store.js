bdcbcb12c9f1b4250dd818870d32543569583ded
Stacy B <stacywdeveloper@gmail.com>
2024-01-08 23:03:32 +0200

    imported thunk correct

--- redux/6-lesson-async-actions/task-2/src/store.js
+++ redux/6-lesson-async-actions/task-2/src/store.js
@@ -1,18 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import usersReducer from './users/users.reducer';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 const reducer = combineReducers({
   users: usersReducer,
 });
-const logger = (store) => (next) => (action) => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  let result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd();
-  return result;
-};
 
 const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhacers(applyMiddleware(thunk)));
